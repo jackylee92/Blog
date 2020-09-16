2bab244e5d76b53fe86b687ada522da5964e3e82
lijundong <jackylee92@139.com>
2020-09-16 15:02:56 +0800

    dcker

--- Docker/docker开发环境/doc/docker-env/cpconf.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker cp 8968f2088de7:/usr/local/etc/php/php.ini-development          /d/docker-env/conf/php/phpconf/
-docker cp 8968f2088de7:/usr/local/etc/php/php.ini-production         /d/docker-env/conf/php/phpconf/
-docker cp 8968f2088de7:/usr/local/etc/php/conf.d        /d/docker-env/conf/php/phpconf/
-docker cp 8968f2088de7:/usr/local/etc/php-fpm.conf /d/docker-env/conf/php/fpmconf/php-fpm.conf
-docker cp 8968f2088de7:/usr/local/etc/php-fpm.d    /d/docker-env/conf/php/fpmconf
--- Docker/docker开发环境/doc/docker-env/zydev.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-export CODEDIR=/d/code
-export LOGDIR=/d/log
-export DOCKER_NGINX_CONFDIR=/d/docker-env/conf
-export DOCKER_NGINX_LOGDIR=/d/docker-env/logs
-export SYSTEM_CONFDIR=/d/code/Config/tars
-
-docker-compose -f docker-compose.yml up -d
